5446afb54e509e5f50a8f6e13b0df282b545581f
Nicolás <78237583+SirNico6@users.noreply.github.com>
2024-11-27 21:59:29 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -1,32 +1,56 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from enum import Enum as PyEnum
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'USER'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Follower(Base):
+    __tablename__ = 'FOLLOWER'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    user_from_id = Column(Integer, ForeignKey('USER.id'))
+    user_to_id = Column(Integer, ForeignKey('USER.id'))
+   
     def to_dict(self):
         return {}
+    
+class Post(Base):
+    __tablename__ = 'POST'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('USER.id'))
+    author = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'COMMENT'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer, ForeignKey('USER.id'))
+    post_id = Column(Integer, ForeignKey('POST.id'))
+    author = relationship(User)
+
+class MediaType(PyEnum):
+    STORY = 'STORY'
+    REEL = 'REEL'
+    PICTURE = 'PICTURE'
+    
+class Media(Base):
+    __tablename__ = 'MEDIA'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum(MediaType))
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('POST.id'))
 
 ## Draw from SQLAlchemy base
 try:
